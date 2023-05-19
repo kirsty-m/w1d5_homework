705dcfc139f8ae13646bb541f838b1fac9daea37
Kirsty McBride <kirsty@kirstys-mbp.home>
2023-05-19 12:36:13 +0100

    13 passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,3 +1,5 @@
+import pdb
+
 # WRITE YOUR FUNCTIONS HERE
 
 def get_pet_shop_name(pet_shop):
@@ -10,3 +12,6 @@ def find_pet_by_name(pet_shop, pet_name):
     for pet in pet_shop["pets"]:
         if pet["name"] == pet_name:
             return pet
+
+def add_or_remove_cash(pet_shop, money_added):
+    pet_shop["admin"]["total_cash"] += money_added
