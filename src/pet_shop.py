4b2d94a857542d035e2894cdb4550f77c2a873cb
Kirsty McBride <kirsty@kirstys-mbp.home>
2023-05-19 11:38:08 +0100

    3 tests passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -5,3 +5,8 @@ def get_pet_shop_name(pet_shop):
 
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
+
+def find_pet_by_name(pet_shop, pet_name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == pet_name:
+            return pet
