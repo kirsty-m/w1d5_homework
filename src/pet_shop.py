b7e90975aee19bcc5af97bc5ac3e4fbd0ed2e59f
Kirsty McBride <kirsty@kirstys-mbp.home>
2023-05-21 13:58:01 +0100

    19 tests passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -64,3 +64,5 @@ def add_pet_to_customer(customer, pet):
 def customer_can_afford_pet(customer, new_pet):
     if customer["cash"] >= new_pet["price"]:
         return True
+    else:
+        return False
